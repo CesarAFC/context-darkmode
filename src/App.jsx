5481933b8ea52bdee038fcc03dce222b74fed65a
Cesar Fontalvo <cafc2030@gmail.com>
2023-04-11 15:10:03 -0500

    Fin context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
 import MyPage from './components/MyPage'
 import MyPageContext from './components/MyPageContext'
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
 
 function App() {
 
   return (
-    <div className='md:container md:mx-auto'>
-      <MyPageContext/>
-      <hr />
-      <MyPage/>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <div className="">
+        <MyPageContext />
+        <hr />
+        <MyPage />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App
